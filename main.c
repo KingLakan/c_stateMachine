983200f4d1fea860eeb4bc8cd5892721f49786b1
David Samuelsson <david.pb.samuelsson@gmail.com>
2023-01-07 21:56:51 +0100

    redo

--- main.c
+++ main.c
@@ -10,21 +10,21 @@ myEvent my_event;
 
 void *eventProducer(void *id) {
   int testInteger = 1;
-//   while (testInteger != 99) {
-//     printf("Enter an integer: \n");
-//     scanf("%d", &testInteger);
-//     my_event.event_a = testInteger;
-//   }
-    while(testInteger<4)
-    {
-      my_event.event_a = testInteger;
-      testInteger++;
-      if (testInteger == 4){
-        testInteger = 0;
-      }
-    //   printf("producer %d\n", my_event.event_a);
-      sleep(2);
-    }
+  while (testInteger != 99) {
+    printf("Enter an integer: \n");
+    scanf("%d", &testInteger);
+    my_event.event_a = testInteger;
+  }
+    // while(testInteger<4)
+    // {
+    //   my_event.event_a = testInteger;
+    //   testInteger++;
+    //   if (testInteger == 4){
+    //     testInteger = 0;
+    //   }
+    // //   printf("producer %d\n", my_event.event_a);
+    //   sleep(2);
+    // }
 }
 
 int main(void) {
--- stateMachine.h
+++ stateMachine.h
@@ -11,6 +11,6 @@ void initStateMachine();
 //void changeState(state newState);
 void currentState();
 void getCurrentStateAdress();
-void sm_next();
+void sm_next(state);
 
 #endif /* B743ADFF_E728_42C5_93A8_443724049E91 */
